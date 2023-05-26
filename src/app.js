ab3bbf896d7baf075e528aed6cf29ababe1a9918
Ali Rojas Cuevas <alirojascuevas@gmail.com>
2023-05-26 11:47:43 -0400

    terminado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT  || 3000;
+const cors =require("cors");
+const router = require('./routes/MailServer');
+ 
 
+
+const port = process.env.PORT || 5000;
+
+app.use (express.json());
+app.use (express.urlencoded({extended : true}));
+
+app.use(cors());
+app.use("/api/mail",router);
+
+
+
+app.get("/", (req, res)=>{
+    res.send("pagina principal");
+})
 
 app.listen(port, ()=>{
 
